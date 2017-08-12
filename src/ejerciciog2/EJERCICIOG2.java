c0b51a9b00017d6686605b0171e4e368a644003b
xxxREIDENxxx <reiden-3@hotmail.com>
2017-08-12 12:02:44 -0500

    commit inicial o inicio de software

---
+++ src/ejerciciog2/EJERCICIOG2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciog2;
+
+/**
+ *
+ * @author arley
+ */
+public class EJERCICIOG2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("HOLA MUNDO DESDE GIT");
+    }
+    
+}
